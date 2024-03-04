2ed376a4188e3ae552a11f884376edd0eeb55f1e
davidselvam <smilingdavid001@gmail.com>
2024-03-04 22:19:37 +0530

    edited : using while loop finding factorial

--- factorial.py
+++ factorial.py
@@ -6,3 +6,13 @@ for i in range(1,x+1):
     total = total * i
     print(total)
 
+# using while loop find factorial
+y = int(input("Enter a number :"))
+total1 = 1
+count = 1
+while count <= y:
+    total1 = total1 * count
+    print(total1)
+    count +=1
+
+
