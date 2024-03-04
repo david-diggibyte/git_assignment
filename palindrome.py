75dbdf56c8de966a5a2d3a1dcebc4e738828ea88
davidselvam <smilingdavid001@gmail.com>
2024-03-04 23:21:32 +0530

    Edited:edited variable name in david1 branch im palidrome file

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,7 @@
 # Find the word is palindrome or not
 
-n = str(input("Enter a name :"))
-if n == n[::-1]:
+name = str(input("Enter a name :"))
+if name == name[::-1]:
     print("Palindrome !!!")
 else:
     print(" Not Palindrome...")
