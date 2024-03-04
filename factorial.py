55fa862dfe0e6c49a5f248511b8dc9ecdc9ded31
davidselvam <smilingdavid001@gmail.com>
2024-03-04 22:28:36 +0530

    edited : edited variable names in factorial file

--- factorial.py
+++ factorial.py
@@ -1,16 +1,16 @@
 # Find the factorial
 # using for loop
-x = int(input("Enter a number :"))
+num = int(input("Enter a number :"))
 total = 1
-for i in range(1,x+1):
+for i in range(1,num+1):
     total = total * i
     print(total)
 
 # using while loop find factorial
-y = int(input("Enter a number :"))
+num1 = int(input("Enter a number :"))
 total1 = 1
 count = 1
-while count <= y:
+while count <= num1:
     total1 = total1 * count
     print(total1)
     count +=1
